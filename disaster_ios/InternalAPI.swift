6c3df0882785afbeff05931d78ac45796459101e
kcaiueo <katsuochan69@i.softbank.jp>
2017-01-21 17:49:44 +0900

    APIとりあえずURLだけ

---
+++ disaster_ios/InternalAPI.swift
@@ -0,0 +1,11 @@
+//
+//  InternalAPI.swift
+//  disaster_ios
+//
+//  Created by WKC on 2017/01/21.
+//  Copyright © 2017年 WKC. All rights reserved.
+//
+
+import Foundation
+
+//http://test.core.its-mo.com/zmaps/api/storenaviweb/cgi/store_nearsearch.cgi?cid=lifeline&lat=35.6863167&lon=139.6950472&ewdist=5000&sndist=5000&pos=1&cnt=100&if_clientid=JSZ74a2ce559215|ePej_&if_auth_type=none
---
+++ disaster_ios/TwitterAPI.swift
@@ -0,0 +1,12 @@
+//
+//  TwitterAPI.swift
+//  disaster_ios
+//
+//  Created by WKC on 2017/01/21.
+//  Copyright © 2017年 WKC. All rights reserved.
+//
+
+import Foundation
+
+
+//url = http://153.120.62.197/cruster/twitterData.php

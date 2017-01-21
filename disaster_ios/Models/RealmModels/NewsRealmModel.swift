a9670ac1613fc0dd6c3ca15c62885d3084fec6c9
atmiyake <atmiyake@yahoo-corp.jp>
2017-01-21 20:17:55 +0900

    Realm 用 Model 実装

---
+++ disaster_ios/Models/RealmModels/NewsRealmModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewsRealmModel.swift
+//  disaster_ios
+//
+//  Created by 三宅　篤史 on 2017/01/21.
+//  Copyright © 2017年 WKC. All rights reserved.
+//
+
+import Foundation
+
+class NewsRealmModel: RealmModel {
+    dynamic var text = ""
+}
---
+++ disaster_ios/SyncEngine/SyncEngine.swift
@@ -0,0 +1,9 @@
+//
+//  SyncEngine.swift
+//  disaster_ios
+//
+//  Created by 三宅　篤史 on 2017/01/21.
+//  Copyright © 2017年 WKC. All rights reserved.
+//
+
+import Foundation

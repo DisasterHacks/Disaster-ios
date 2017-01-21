558e7fa7054a6bb0ae6ca190ab0b850e00a8bd29
atmiyake <atmiyake@yahoo-corp.jp>
2017-01-21 22:16:28 +0900

    Shelter 用の RealmModel を追加

---
+++ disaster_ios/Models/RealmModels/ShelterRealmModel.swift
@@ -0,0 +1,13 @@
+//
+//  ShelterRealmModel.swift
+//  disaster_ios
+//
+//  Created by 三宅　篤史 on 2017/01/21.
+//  Copyright © 2017年 WKC. All rights reserved.
+//
+
+import Foundation
+
+class ShelterRealmModel: RealmModel {
+    dynamic var name = ""
+}

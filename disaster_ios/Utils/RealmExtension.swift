1dcce6d54e8acd52a59a5a227bc186ed40be8797
atmiyake <atmiyake@yahoo-corp.jp>
2017-01-21 20:19:13 +0900

    RealmExtension 追加

--- disaster_ios/Utils/RealmExtension.swift
+++ disaster_ios/Utils/RealmExtension.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+import RealmSwift
+
+extension Realm {
+    class func objects<T: Object>(_ object: T.Type) -> Results<T> {
+        let realm = try! Realm()
+        return realm.objects(T.self)
+    }
+    
+    class func addObject<T: Object>(_ model: T) {
+        let realm = try! Realm()
+        try! realm.write({
+            realm.add(model, update: true)
+        })
+    }
+    
+    class func updateObject<T: Object>(_ model: T) {
+        let realm = try! Realm()
+        try! realm.write({
+            realm.add(model, update: true)
+        })
+    }
+    
+    class func deleteObject<T: Object>(_ model: T) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.delete(model)
+        }
+    }
+    
+    class func deleteAll<T: Object>(_ type: T.Type) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.delete(realm.objects(type))
+        }
+    }
+}

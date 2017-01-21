25050add147b3b335be348c293f3a7049c81d073
atmiyake <atmiyake@yahoo-corp.jp>
2017-01-21 17:31:58 +0900

    プロトコルに準拠した最低限の実装

--- disaster_ios/MeshEngine/MeshEngine.swift
+++ disaster_ios/MeshEngine/MeshEngine.swift
@@ -13,3 +13,32 @@ protocol MeshEngineable: class {
     var peerId: MCPeerID { get set }
     func send<T: Syncable>(syncable: T)
 }
+
+/// Mesh Network 上のデータ I/O イベント管理クラス
+class MeshEngine: NSObject, MeshEngineable {
+    
+    // singleton
+    static let shared: MeshEngine = MeshEngine()
+    
+    // peer
+    var peerId: MCPeerID = MCPeerID(displayName: UUID.init().uuidString)
+    
+    var session: MCSession
+
+    private override init() {
+        self.session = MCSession(peer: self.peerId, securityIdentity: nil, encryptionPreference: .none)
+        super.init()
+    }
+    
+    func send<T: Syncable>(syncable: T) {
+        guard let data = syncable.encode() else {
+            return
+        }
+        
+        do {
+            try self.session.send(data, toPeers: self.session.connectedPeers, with: .reliable)
+        } catch {
+            print("send error")
+        }
+    }
+}
